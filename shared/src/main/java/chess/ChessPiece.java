23ec1b4e2550852c5ac42a7b2b28be51f8a5c67a
Justin <palmerjustins@outlook.com>
2025-09-18 23:39:33 -0600

    7/8! We are getting close to a breakthrough, I can feel it

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -3,7 +3,6 @@ package chess;
 import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
-import java.util.
 
 /**
  * Represents a single chess piece
@@ -11,7 +10,7 @@ import java.util.
  * Note: You can add to this class, but you may not alter
  * signature of the existing methods.
  */
-public class ChessPiece implements PieceMoveCalculator{
+public class ChessPiece<E> implements PieceMoveCalculator<E> {
 
     private final ChessGame.TeamColor pieceColor;
     private final PieceType type;
@@ -55,39 +54,31 @@ public class ChessPiece implements PieceMoveCalculator{
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        PieceMoveCalculator calculate;
 
-        if (type == ChessPiece.PieceType.KING) {
-            calculate = new KingMovesCalculator();
-        }
-
-        if (type == ChessPiece.PieceType.KNIGHT) {
-            calculate = new KnightMovesCalculator();
-        }
-
-        if (type == ChessPiece.PieceType.QUEEN) {
-            calculate = new QueenMocesCalculator();
-        }
+//        if (type == ChessPiece.PieceType.KING) {
+//            calculate = new KingMovesCalculator();
+//        }
+//
+//        if (type == ChessPiece.PieceType.KNIGHT) {
+//            calculate = new KnightMovesCalculator();
+//        }
+//
+//        if (type == ChessPiece.PieceType.QUEEN) {
+//            calculate = new QueenMocesCalculator();
+//        }
 
         if (type == ChessPiece.PieceType.BISHOP) {
-            calculate = new BishopMovesCalculator();
+            BishopMovesCalculator<E> calculate = new BishopMovesCalculator<E>();
+            return calculate.pieceMoves(board, myPosition);
         }
 
-        if (type == ChessPiece.PieceType.ROOK) {
-            calculate = new RookMovesCalculator();
-        }
-
-        if (type == ChessPiece.PieceType.PAWN) {
-            calculate = new PawnMovesCalculator();
-        }
-
-
-
-
-
-
-
-
+//        if (type == ChessPiece.PieceType.ROOK) {
+//            calculate = new RookMovesCalculator();
+//        }
+//
+//        if (type == ChessPiece.PieceType.PAWN) {
+//            calculate = new PawnMovesCalculator();
+//        }
 
 
         //        ChessPiece piece = board.getPiece(myPosition);
@@ -95,7 +86,9 @@ public class ChessPiece implements PieceMoveCalculator{
 //            return List.of(new ChessMove(new ChessPosition(5,4), new ChessPosition(1,8), null));
 //        }
 //        return List.of();
-//    }
+        else{
+            return null;
+        }
     }
 
     @Override
