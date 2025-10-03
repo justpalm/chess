167f7020645f4fe3e552830a43205ec82c247ab8
Justin <palmerjustins@outlook.com>
2025-10-02 18:50:12 -0600

    I think this is five? or four? I've made a lot of progress on moving on with the checking, I'll have to check it more

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -61,30 +61,25 @@ public class ChessGame {
         BLACK
     }
 
-    /**
-     * Gets a valid moves for a piece at the given location
-     *
-     * @param startPosition the piece to get valid moves for
-     * @return Set of valid moves for requested piece, or null if no piece at
-     * startPosition
-     */
-
-    private Collection<ChessMove> check_check(ChessPiece piece, Collection<ChessMove> og_valid_moves, ChessBoard copy_of_old) {
+    private Collection<ChessMove> check_check(ChessPiece piece, Collection<ChessMove> og_valid_moves) {
 
         Collection<ChessMove> new_valid_moves = new ArrayList<>();
 
-
+        //Go through every move in the valid set to see the hypotheticals
         for (ChessMove move : og_valid_moves) {
-            ChessBoard new_old = copy_of_old;
-            new_old.addPiece(move.getEndPosition(), piece);
-            new_old.addPiece(move.getStartPosition(), null);
+            //Make a copy of the
+            ChessBoard copy_of_og_board = game_board;
 
+            copy_of_og_board.addPiece(move.getEndPosition(), piece);
+            copy_of_og_board.addPiece(move.getStartPosition(), null);
+
+            //Here's us checking if this will cause the King to be in check
             for (int row = 1; row < 9; row++) {
                 for (int col = 1; col < 9; col++) {
                     ChessPosition new_position = new ChessPosition(row, col);
-                    if (new_old.getPiece(new_position) != null) {
-                        ChessPiece new_piece = new_old.getPiece(new_position);
-                        Collection<ChessMove> new_piece_moves = new_piece.pieceMoves(new_old, new_position);
+                    if (copy_of_og_board.getPiece(new_position) != null & game_board.getPiece(new_position).getTeamColor() == OppositeTeam) {
+                        ChessPiece new_piece = copy_of_og_board.getPiece(new_position);
+                        Collection<ChessMove> new_piece_moves = new_piece.pieceMoves(copy_of_og_board, new_position);
                         for (ChessMove new_piece_move : new_piece_moves) {
                             if (CurrentTeam != TeamColor.WHITE) {
                                 if (new_piece_move.getEndPosition() != WhiteKing) {
@@ -101,7 +96,7 @@ public class ChessGame {
                 }
             }
         }
-    return new_valid_moves;
+        return new_valid_moves;
     }
 // Make a copy of the old board, and then on the old board, move the piece
 // From there, have it run through all the piece calculator stuff
@@ -111,19 +106,29 @@ public class ChessGame {
 // If it does put the Same teams color in check, return edited.
 
 
+
+
+    /**
+     * Gets a valid moves for a piece at the given location
+     *
+     * @param startPosition the piece to get valid moves for
+     * @return Set of valid moves for requested piece, or null if no piece at
+     * startPosition
+     */
+
+
 public Collection<ChessMove> validMoves(ChessPosition startPosition) {
     if (game_board.getPiece(startPosition) != null) {
         ChessPiece piece = game_board.getPiece(startPosition);
         Collection<ChessMove> og_valid_moves = piece.pieceMoves(game_board, startPosition);
-        ChessBoard copy_of_old = game_board;
-        Collection<ChessMove> new_valid_moves = check_check(piece, og_valid_moves, copy_of_old);
+        Collection<ChessMove> new_valid_moves = check_check(piece, og_valid_moves);
 
         return new_valid_moves;
-        else{
+    }
+        else {
             return null;
         }
     }
-}
 
 /**
  * Makes a move in a chess game
@@ -138,12 +143,23 @@ public void makeMove(ChessMove move) throws InvalidMoveException {
 
     if (valid_moves.contains(move)) {
 
+        //Do the move by adding and removing whatever pieces we have to
         game_board.addPiece(move.getEndPosition(), game_board.getPiece(move.getStartPosition()));
         game_board.addPiece(move.getStartPosition(), null);
 
+        //This is to check to update our King Position
+        if (game_board.getPiece(move.getEndPosition()).getPieceType() == ChessPiece.PieceType.KING)
+            if (CurrentTeam == TeamColor.WHITE) {
+                WhiteKing = move.getEndPosition();
+            }
+            else {
+                BlackKing = move.getEndPosition();
+            }
+
+
     }
     else {
-        throw new InvalidMoveException("Invalid move");
+        throw new InvalidMoveException("Invalid move requested");
     }
 
 }
@@ -155,20 +171,40 @@ public void makeMove(ChessMove move) throws InvalidMoveException {
  * @return True if the specified team is in check
  */
 public boolean isInCheck(TeamColor teamColor) {
+    boolean state = false;
+    //Copied from check_Check, the logic is slightly different though as it wants a positive rather than a negative
+    //And doesn't make a new list
+        for (int row = 1; row < 9; row++) {
+            for (int col = 1; col < 9; col++) {
+                ChessPosition new_position = new ChessPosition(row, col);
+                if (game_board.getPiece(new_position) != null & game_board.getPiece(new_position).getTeamColor() == OppositeTeam) {
+                    ChessPiece new_piece = game_board.getPiece(new_position);
+                    Collection<ChessMove> new_piece_moves = new_piece.pieceMoves(game_board, new_position);
+                    for (ChessMove new_piece_move : new_piece_moves) {
+                        if (CurrentTeam != TeamColor.WHITE) {
+                            if (new_piece_move.getEndPosition() == WhiteKing) {
+                                state = true;
+                            }
+                        }
+                        if (CurrentTeam != TeamColor.BLACK) {
+                            if (new_piece_move.getEndPosition() == BlackKing) {
+                                state = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    return true;
+}
+
+
     // Cycle through every piece on the board
     // If there's an enemy piece, see if they have moves on the King
     // If it goes through the whole board and no one can get him,
     // no in Check.
 
     //If it can, then check
-
-
-
-
-
-
-    throw new RuntimeException("Not implemented");
-}
 
 /**
  * Determines if the given team is in checkmate
@@ -207,6 +243,8 @@ public boolean isInStalemate(TeamColor teamColor) {
 public void setBoard(ChessBoard board) {
     game_board = board;
     game_board.resetBoard();
+    WhiteKing = new ChessPosition(1,5);
+    BlackKing = new ChessPosition(8, 5);
 
 }
 
