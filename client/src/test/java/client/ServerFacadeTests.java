73de2c3583da7cfd9c00d560d0e1d069ec8283cc
Justin <palmerjustins@outlook.com>
2025-11-13 22:42:55 -0700

    0 for the port

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -28,7 +28,7 @@ public class ServerFacadeTests {
     @BeforeAll
     public static void init() throws DataAccessException{
         server = new Server();
-        var port = server.run(8080);
+        var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
         facade = new ServerFacade(String.format("http://localhost:%d", port));
     }
