4e2bfb4f607e1590f8905519ae4bc78a3cf7de70
Justin <palmerjustins@outlook.com>
2025-11-19 18:31:14 -0700

    Here's this fix

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -3,8 +3,6 @@
 
 import chess.*;
 import dataaccess.exceptions.DataAccessException;
-import server.Server;
-import serverfacade.ServerFacade;
 import ui.Repl;
 import ui.client.BaseChessBoardDrawing;
 import ui.client.BlackChessBoardDrawing;
