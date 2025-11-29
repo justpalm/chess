e2ee4426545930729705d55ea912a36c30e5fa26
Justin <palmerjustins@outlook.com>
2025-11-29 13:44:13 -0700

    We are getting close amigos!

--- server/src/main/java/websocket/WebSocketHandler.java
+++ server/src/main/java/websocket/WebSocketHandler.java
@@ -1,5 +1,4 @@
-package websocket;
-
+package server.websocket;
 
 import com.google.gson.Gson;
 import exception.ResponseException;
@@ -10,25 +9,60 @@ import io.javalin.websocket.WsConnectHandler;
 import io.javalin.websocket.WsMessageContext;
 import io.javalin.websocket.WsMessageHandler;
 import org.eclipse.jetty.websocket.api.Session;
-import org.jetbrains.annotations.NotNull;
 import webSocketMessages.Action;
 import webSocketMessages.Notification;
 
+import java.io.IOException;
 
+public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsCloseHandler {
 
-public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsCloseHandler{
+    private final ConnectionManager connections = new ConnectionManager();
+
     @Override
-    public void handleClose(@NotNull WsCloseContext wsCloseContext) throws Exception {
-
+    public void handleConnect(WsConnectContext ctx) {
+        System.out.println("Websocket connected");
+        ctx.enableAutomaticPings();
     }
 
     @Override
-    public void handleMessage(@NotNull WsMessageContext wsMessageContext) throws Exception {
-
+    public void handleMessage(WsMessageContext ctx) {
+        try {
+            Action action = new Gson().fromJson(ctx.message(), Action.class);
+            switch (action.type()) {
+                case ENTER -> enter(action.visitorName(), ctx.session);
+                case EXIT -> exit(action.visitorName(), ctx.session);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 
     @Override
-    public void handleConnect(@NotNull WsConnectContext wsConnectContext) throws Exception {
+    public void handleClose(WsCloseContext ctx) {
+        System.out.println("Websocket closed");
+    }
 
+    private void enter(String visitorName, Session session) throws IOException {
+        connections.add(session);
+        var message = String.format("%s is in the shop", visitorName);
+        var notification = new Notification(Notification.Type.ARRIVAL, message);
+        connections.broadcast(session, notification);
+    }
+
+    private void exit(String visitorName, Session session) throws IOException {
+        var message = String.format("%s left the shop", visitorName);
+        var notification = new Notification(Notification.Type.DEPARTURE, message);
+        connections.broadcast(session, notification);
+        connections.remove(session);
+    }
+
+    public void makeNoise(String petName, String sound) throws ResponseException {
+        try {
+            var message = String.format("%s says %s", petName, sound);
+            var notification = new Notification(Notification.Type.NOISE, message);
+            connections.broadcast(null, notification);
+        } catch (Exception ex) {
+            throw new ResponseException(ResponseException.Code.ServerError, ex.getMessage());
+        }
     }
 }
